67c820394f9e0cb4570a596bb4809b7f0126f0e9
Tim Pease <tim.pease@gmail.com>
2012-04-05 13:25:46 -0600

    cleaning up initial specs

--- spec/rall_spec.rb
+++ spec/rall_spec.rb
@@ -1,6 +1,7 @@
 
-require File.join(File.dirname(__FILE__), %w[spec_helper])
+require File.expand_path('../spec_helper', __FILE__)
 
 describe Rall do
+  it 'needs some specs'
 end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,7 @@
 
-require File.expand_path(
-    File.join(File.dirname(__FILE__), %w[.. lib rall]))
+require File.expand_path('../../lib/rall', __FILE__)
 
-Spec::Runner.configure do |config|
+RSpec.configure do |config|
   # == Mock Framework
   #
   # RSpec uses it's own mocking framework by default. If you prefer to
