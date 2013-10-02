0445368d733e44b0f0afd5d7908270544f4b7385
Tim Pease <tim.pease@gmail.com>
2013-10-02 10:27:24 -0600

    Validate the provider when registering.

--- lib/polylog.rb
+++ lib/polylog.rb
@@ -8,13 +8,28 @@ module Polylog
     provider.logger name
   end
 
+  # Return the currently selected provider. If a provider has not yet been
+  # specified, then the "null" provider will be used.
   #
+  # Returns a logger provider.
   def provider
     use_provider('null') unless defined? @provider
     @provider
   end
 
+  # Returns the list of available providers as an Array of Strings. These
+  # names are used to select which provider to use.
+  def providers
+    @providers.keys
+  end
+
+  # Configure the logger provider that will be used by Polylog. The provider
+  # must already be registered using the same `name`.
   #
+  # name - The provider name as a String.
+  #
+  # Returns the logger provider.
+  # Raises a Polylog::UnknownProvider exception.
   def use_provider( name )
     name = name.to_s
     raise Polylog::UnknownProvider, "unkonwn provider: #{name.inspect}" unless @providers.key? name
@@ -22,15 +37,32 @@ module Polylog
     @provider = @providers[name]
   end
 
+  # Register a logger provider under the given name. The provider can be any
+  # Ruby object that responds to the `logger` method and returns valid logger
+  # instances. An exception is raised if the provider does not respond to the
+  # `logger` method or the `logger` method does not have an arity of 1.
   #
+  # name     - The provider name as a String.
+  # provider - The provider object that responds to the `logger` method.
+  #
+  # Returns the logger provider.
+  # Raises a Polylog::InvalidProvider exception.
   def register_provider( name, provider )
     @providers ||= Hash.new
+
+    unless provider.respond_to?(:logger) && provider.method(:logger).arity == 1
+      raise Polylog::InvalidProvider, '`logger` method not found or has wrong arity'
+    end
 
     name = name.to_s
     @providers[name] = provider
   end
 
+  # Internal:
   #
+  # object - Any ruby object
+  #
+  # Returns the logger name String or nil.
   def logger_name( object )
     case object
     when nil, String; object
@@ -42,6 +74,9 @@ module Polylog
 
   # Internal:
   #
+  # mod - A Module or Class
+  #
+  # Returns the logger name String or nil.
   def logger_name_for_module( mod )
     return mod.name unless mod.name.nil? or mod.name.empty?
 
--- lib/polylog/errors.rb
+++ lib/polylog/errors.rb
@@ -7,4 +7,9 @@ module Polylog
   # This is error is raised when an unknown provider is requested by the user.
   UnknownProvider = Class.new Error
 
+  # This is error is raised when a provider is registered and it does not
+  # respond to the `logger` method or the `logger` method has an arity other
+  # than 1.
+  InvalidProvider = Class.new Error
+
 end
