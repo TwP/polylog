2f97be1348353616078358f584e0c859d4c77667
Tim Pease <tim.pease@gmail.com>
2013-10-14 09:44:32 -0600

    adding those tests

--- spec/polylog_spec.rb
+++ spec/polylog_spec.rb
@@ -2,6 +2,21 @@
 require File.expand_path('../spec_helper', __FILE__)
 
 describe Polylog do
-  it 'needs some specs'
+  context 'without any configuration' do
+    it 'provides a null logger' do
+      expect(Polylog.logger).to be_an_instance_of Polylog::NullLogger
+    end
+
+    it 'provides the smae null logger for all requests' do
+      null_logger = Polylog.logger
+
+      expect(Polylog.logger 'foo').to equal null_logger
+      expect(Polylog.logger 'bar').to equal null_logger
+    end
+
+    it 'only has the null provider registered' do
+      expect(Polylog.providers).to eq %w[null]
+    end
+  end
 end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,8 @@
 require File.expand_path('../../lib/polylog', __FILE__)
 
 RSpec.configure do |config|
+  config.before(:all) { Polylog.reset! }
+
   # == Mock Framework
   #
   # RSpec uses it's own mocking framework by default. If you prefer to
@@ -10,5 +12,20 @@ RSpec.configure do |config|
   # config.mock_with :mocha
   # config.mock_with :flexmock
   # config.mock_with :rr
+end
+
+module Polylog
+  # Only used for testing. This resets the Polylog providers configuration to
+  # its uninitialized state.
+  def reset!
+    remove_instance_variable :@provider  if defined? @provider
+
+    @providers.keys.each do |key|
+      next if key == 'null'
+      @providers.delete key
+    end
+
+    self
+  end
 end
 
